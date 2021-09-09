744c5a7f4978970b77d30dcfb2c4993b9f1ba39e
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-09-09 22:16:34 +0200

    added search and add recipe function that stores to db

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var express = require('express');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const auth = require('./routes/auth.routes');
+const search = require('./routes/search.routes')
 
 var app = express();
 
@@ -17,6 +18,7 @@ require('./config/global')(app)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', auth);
+app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/search.routes.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const express = require('express');
+const router = express.Router();
+const Recipe = require('../models/Recipe.model');
+const User = require('../models/User.model');
+
+router.get('/ingredients', (req,res) => {
+    res.render('search-recipes');
+});
+
+router.post('/ingredients', (req,res) => {
+    const { ingredients } = req.body
+    concatIngredients = ingredients.split(',').reduce((prev, curr) => {
+    return prev + ',+' + curr;
+    })
+    axios({
+        method: 'get',
+        url: `https://api.spoonacular.com/recipes/findByIngredients?apiKey=60dcb55702f14e43ab172bf6ff6ac5a4&ingredients=${concatIngredients}&ranking=2&ignorePantry=true`
+        // url: `https://api.spoonacular.com/recipes/findByIngredients?apiKey=60dcb55702f14e43ab172bf6ff6ac5a4&ingredients=apples,+flour,+sugar&number=2`
+    })
+    .then(data => res.render('search-results', {data: data.data}))
+    .catch(err => console.log(err));
+})
+
+router.get('/save/recipe/:id', (req,res) => {
+    axios({
+        method: 'get',
+        url: `https://api.spoonacular.com/recipes/${req.params.id}/information?apiKey=60dcb55702f14e43ab172bf6ff6ac5a4&includeNutrition=false`
+    })
+    .then(data => {
+        const allIngredients = []
+        for(let ingredient in data.data.extendedIngredients) {
+            allIngredients.push(data.data.extendedIngredients[ingredient].name);
+        };
+        Recipe.create({ apidDBId: req.params.id,image: data.data.image, title: data.data.title ,ingredients: allIngredients})
+        .then(recipe => console.log(recipe))
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+})
+
+module.exports = router;
