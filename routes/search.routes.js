2549afcdefcb40d5ff662532fe4bbc790b808c78
Coops023 <bjorkc27@gmail.com>
2021-09-10 13:55:24 +0200

    working on api call

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -4,42 +4,61 @@ const router = express.Router();
 const Recipe = require('../models/Recipe.model');
 const User = require('../models/User.model');
 
-router.get('/ingredients', (req,res) => {
+router.get('/ingredients', (req, res) => {
     res.render('search-recipes');
 });
 
-router.post('/ingredients', (req,res) => {
+router.post('/ingredients', (req, res) => {
     const { ingredients } = req.body
     concatIngredients = ingredients.split(',').reduce((prev, curr) => {
-    return prev + ',+' + curr;
+        return prev + ',+' + curr;
     })
     axios({
         method: 'get',
         url: `https://api.spoonacular.com/recipes/findByIngredients?apiKey=60dcb55702f14e43ab172bf6ff6ac5a4&ingredients=${concatIngredients}&ranking=2&ignorePantry=true`
     })
-    .then(data => res.render('search-results', {data: data.data}))
-    .catch(err => console.log(err));
+        .then(data => res.render('search-results', { data: data.data }))
+        .catch(err => console.log(err));
 })
 
-router.get('/save/recipe/:id', (req,res) => {
+router.get('/save/recipe/:id', (req, res) => {
     axios({
         method: 'get',
         url: `https://api.spoonacular.com/recipes/${req.params.id}/information?apiKey=60dcb55702f14e43ab172bf6ff6ac5a4&includeNutrition=false`
     })
-    .then(data => {
-        const allIngredients = []
-        for(let ingredient in data.data.extendedIngredients) {
-            allIngredients.push(data.data.extendedIngredients[ingredient].name);
-        };
-        Recipe.create({ apidDBId: req.params.id,image: data.data.image, title: data.data.title ,ingredients: allIngredients})
-        .then(recipe => 
-            User.findByIdAndUpdate(req.session.currentUser._id, {$addToSet: { recipes: recipe._id }})
-            .then(() => console.log('recipe saved to user!'))
-            .catch(err => console.log(err))
-        )
+        .then(data => {
+            const allIngredients = []
+            for (let ingredient in data.data.extendedIngredients) {
+                allIngredients.push(data.data.extendedIngredients[ingredient].name);
+            };
+            Recipe.create({ apidDBId: req.params.id, image: data.data.image, title: data.data.title, ingredients: allIngredients })
+                .then(recipe =>
+                    User.findByIdAndUpdate(req.session.currentUser._id, { $addToSet: { recipes: recipe._id } })
+                        .then(() => console.log('recipe saved to user!'))
+                        .catch(err => console.log(err))
+                )
+                .catch(err => console.log(err));
+        })
         .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
 })
+
+// Cooper API work 
+
+router.get("/recipes", (req, res) => {
+    res.render('cooper-api-search')
+})
+router.post("/recipes", (req, res) => {
+    const { ingredients } = req.body;
+    console.log(ingredients)
+    axios({
+        method: "get",
+        url: `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.API_KEY}&query=${ingredients}`
+    })
+        .then(response => {
+            console.log(response.data.results[1])
+        })
+})
+
+
 
 module.exports = router;
