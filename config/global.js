67559862ec622d9271ed6038be3e3de85598fa8d
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-09-11 10:55:08 +0200

    added bulma and some css styling

--- config/global.js
+++ config/global.js
@@ -14,39 +14,39 @@ const hbs = require('hbs')
 
 // Middleware configuration
 module.exports = (app) => {
-    // In development environment the app logs
-    app.use(logger("dev"));
+  // In development environment the app logs
+  app.use(logger("dev"));
 
-    // To have access to `body` property in the request
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
+  // To have access to `body` property in the request
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
 
-    // Normalizes the path to the views folder
-    app.set("views", [path.join(__dirname, "..", "views"), path.join(__dirname, "..", "views/pages")]);
-    // Sets the view engine to handlebars
-    app.set("view engine", "hbs");
+  // Normalizes the path to the views folder
+  app.set("views", [path.join(__dirname, "..", "views"),path.join(__dirname, "..", "views/pages")]);
+  // Sets the view engine to handlebars
+  app.set("view engine", "hbs");
 
-    hbs.registerPartials(path.join(__dirname, '/views/partials'))
+  hbs.registerPartials(path.join(__dirname, '/views/partials'))
 
-    // Handles access to the public folder
-    app.use(express.static(path.join(__dirname, "..", "public")));
+  // Handles access to the public folder
+  app.use(express.static(path.join(__dirname, "..", "public")));
 
-    // Handles access to the favicon
-    app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
+  // Handles access to the favicon
+  app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
 
-    app.use(
-        session({
-            secret: 'Globtrotters-secret',
-            resave: false,
-            saveUninitialized: true,
-            cookie: {
-                maxAge: 24 * 60 * 60 * 1000
-            },
-            store: MongoStore.create({
-                mongoUrl: `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
-            })
-        })
-    );
+  app.use(
+		session({
+			secret: 'Globtrotters-secret',
+			resave: false,
+			saveUninitialized: true,
+			cookie: {
+				maxAge: 24 * 60 * 60 * 1000
+			},
+			store: MongoStore.create({
+				mongoUrl: `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+			})
+		})
+	);
 
 };
